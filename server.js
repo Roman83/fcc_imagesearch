4b0e70d883efacb59d9fa229d6e63d2c8983823c
Roman Tolstopyatov <r.tolstopyatov@gmail.com>
2017-03-29 22:12:45 +0300

    keep history

--- server.js
+++ server.js
@@ -4,18 +4,32 @@ var app = express();
 var port = process.env.PORT || 8080;
 var key = process.env.KEY;
 var cx = process.env.CX;
-var history = [];
 
-console.log(key);
-console.log(cx);
+var history = function() {
+    var history = [];
+    res = {
+        'update': function (str) {
+            if (history.unshift({'q': str, 'date': new Date()}) > 10) {
+                history.pop();
+            }
+        },
+        'get': function() {
+            return JSON.stringify(history);
+        }
+    };
+    return res;
+}();
 
 app.get('/search/:data', function (req, res) {
 
     var offset = req.query.offset || 1;
+    var q = req.params.data;
     var url = 'https://www.googleapis.com/customsearch/v1?key=' + key +
-            '&q=' + req.params.data +
+            '&q=' + q +
             '&cx=' + cx + '&searchType=image' + 
             '&start=' + offset;
+
+    history.update(q);
 
     http.get(url, function(httpres) {
         var body = '';
@@ -25,6 +39,10 @@ app.get('/search/:data', function (req, res) {
 
         httpres.on('end', function(){
             body = JSON.parse(body);
+            if (!body.items) {
+                res.send('error');
+                return;
+            }
             res.send(body.items.map(function(i) {
                 return {'url': i.link,
                         'alttext': i.title,
@@ -32,12 +50,29 @@ app.get('/search/:data', function (req, res) {
                        };
             }));
         });
-    });
 
+        httpres.on('error', function() {
+            res.send('error');
+        });
+    }).on('error', function() {
+        res.send('error');
+    });
 });
 
 app.get('/latest', function (req, res) {
-    res.send(history);
+    res.send(history.get());
 });
 
 app.listen(port);
+
+
+
+
+
+
+
+
+
+
+
+
